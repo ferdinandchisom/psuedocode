8bddf66dcac4d23b5713f9e1a2bca61aecb5016e
ferdinand chisom <ferdinandchisom7@gmail.com>
2025-02-07 06:11:31 -0800

    updated the task

---
+++ testing.js
@@ -0,0 +1,81 @@
+//1 write a function that print the area of a recangle given it length and weight
+psuedocode;
+
+// Determain area of a recangle
+//multiply length and weight
+//print final answer
+
+//2 Write a function that converts a temperature from Celsius to Fahrenheit and prints the result.
+psuedocode;
+
+//write the given name celsius bind with a const
+//reveser name using a let
+// print final result
+
+//3 Write a function that calculates the factorial of a number and prints the result.
+psuedocode;
+
+//choose a number of the factorial
+//multiply num from the choosen factorial to the last digit given 1
+//print final result
+
+//4 Write a function that checks if a number is prime and prints "Prime" or "Not Prime".
+psuedocode;
+
+//let prime num be 1_8
+//while prime num be 8_10
+// print result
+
+//5 Write a function that calculates the sum of all even numbers between 1 and a given number, then prints the result.
+psuedocode;
+
+//let the sum of all even numbers be 1
+//given numbers should be <= 20
+//print final result
+
+//6 Write a function that calculates the compound interest given principal, rate, time, and number of compounding periods, then prints the result.
+psuedocode;
+
+//let the given principal for time be 1
+//let the given principal for rate be 2
+// Determain the number of time and rate by string
+//print the final result
+
+//7 Write a function that calculates the volume of a sphere given its radius and prints the result.
+psuedocode;
+
+//if the volume of the sphere is =r^8
+//let the number of volume be 10
+//print final result
+
+//8 Write a function that calculates the hypotenuse of a right-angled triangle given the other two sides and prints the result.
+psuedocode;
+
+//Determain the side of the triangle
+//let the first side be given a num of 1
+//let the second side be given a num of 5
+//print the final result
+
+//9: Write a function that calculates the Body Mass Index (BMI) given weight (in kg) and height (in meters), then prints the BMI category (Underweight, Normal, Overweight, Obese).
+
+psuedocode;
+
+//Determain the height and weight
+//if bmi is 20.5 print underweight
+//if bmi is above 20.5 and 28.9 print normal
+// if it is 29.9 and 30 print overweight
+//if it is >30print obese
+
+//10  Write a function that calculates the sum of the digits of a number and prints the result.
+psuedocode;
+
+//Determain the digit of an inputted number
+//sum from 1 to the digit
+
+//14: Write a function that calculates the greatest common divisor (GCD) of two numbers and prints the result.
+psuedocode;
+
+//Determain common numbers of the divisor
+//Divide both numbers without remainders
+//find a number that can divide the two numbers
+//print out result
